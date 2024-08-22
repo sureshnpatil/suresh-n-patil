bd1cdf3a9cd369a51aed347fc6eee2b7c0e85905
sureshnpatil <sureshpatil9353@gmail.com>
2024-08-22 08:09:31 +0530

    Add files via upload

---
+++ python programmes kleit/p2.py
@@ -0,0 +1,6 @@
+#read a number from the user and check if it is an Even number or not
+num=int(input("enter a value"))
+if(num%2==0):
+    print(num,"is even number")
+else:
+    print(num,"is not an Even Number")
